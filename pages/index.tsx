b358e7b046455da3990c983154156c9200d2cf43
Paweł Biegun <biegunpawel900@gmail.com>
2022-06-10 11:51:20 +0200

    connect to the api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from 'next'
+import {  Configuration, DefaultApi } from "../client"
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex">
-        <p className="text-3xl font-semibold">Hello world</p>
+        <p className="text-3xl font-semibold">{props["message"]}</p>
       </div>
     </div>
   )
 }
+
+
+export const getStaticProps = async () => {
+  const config = new Configuration()
+  const api = new DefaultApi(config)
+
+
+  const message = await api.readRootApiGet()
+  console.log(JSON.parse(message))
+
+  return {
+    props: {
+      message: JSON.parse(message).message
+    }
+  }
+}
+
 
 export default Home
