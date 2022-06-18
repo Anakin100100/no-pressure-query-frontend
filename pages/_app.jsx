b77779dd2ba8caae08cf8d5b27bb39c479fa115b
Paweł Biegun <biegunpawel900@gmail.com>
2022-06-18 14:32:59 +0200

    remove needless dependencies

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,11 @@
 import "../styles/globals.css";
 import { UserProvider } from "../contexts/user_context";
 
-import { ChakraProvider } from "@chakra-ui/react";
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-    </ChakraProvider>
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
   );
 }
 
